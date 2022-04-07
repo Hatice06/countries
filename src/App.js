f66145559b9a483afac1299cac22b72a250098b7
Hatice06 <haticeaydogan00@gmail.com>
2022-04-08 00:12:34 +0100

    level 2 works

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import CountryCard from "./CountryCard";
+import Search from "./Search";
+
 
 function App() {
   const [countriesAll, setCountriesAll] = useState([]);
 
-  fetch("https://restcountries.com/v2/all")
-    .then((response) => response.json())
-    .then((data) => setCountriesAll(data));
+  useEffect(() => {
+    fetch("https://restcountries.com/v2/all")
+      .then((response) => response.json())
+      .then((data) => setCountriesAll(data));
+  }, []);
+
+  const handleChange = (event) => {
+    console.log(countriesAll);
+    const filtered = countriesAll.filter((country) =>
+      country.name.toUpperCase().includes(event.target.value.toLowerCase())
+    );
+    console.log(event.target.value);
+    setCountriesAll(filtered);
+    console.log(filtered);
+  };
 
   return (
     <div className="main-container">
-      <div className="search-bar">
-        <input type="text" placeholder="Search for a country..."></input>
-      </div>
+      <Search handleChange={handleChange} />
       <div className="card-container">
         {countriesAll.map((country) => (
           <CountryCard country={country} />
--- src/CountryCard.js
+++ src/CountryCard.js
@@ -3,11 +3,11 @@ import React from "react";
 function CountryCard({ country }) {
   return (
     <div className="country-card">
-      <img src={country.flags.svg} alt="country-flag" />
-      <h3>{country.name}</h3>
+      <img src={country.flag} alt="country-flag" />
+      <div className="country-info"><h3>{country.name}</h3>
       <p><b>Population:</b>{country.population}</p>
       <p><b>Region:</b>{country.region}</p>
-      <p><b>Capital:</b>{country.capital}</p>
+      <p><b>Capital:</b>{country.capital}</p></div>
     </div>
   );
 }
